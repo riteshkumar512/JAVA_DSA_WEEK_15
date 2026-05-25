8b057fb7f9f7e9cb2e3c9267315ab60ac709c152
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-05-25 22:12:28 +0530

    Leetcode Question No: 20

---
+++ src/Lecture_25/LC_20_Valid_Parenthesis.java
@@ -0,0 +1,31 @@
+package Lecture_25;
+
+import java.util.Stack;
+
+public class LC_20_Valid_Parenthesis {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+    public static boolean isValid(String s){
+        Stack<Character> st=new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch=s.charAt(i);
+            if (ch=='{' || ch=='[' || ch=='('){
+                st.push(ch);
+            }else {
+                if (st.isEmpty()){
+                    return false;
+                } else if (ch=='}' && st.peek()!='{') {
+                    return false;
+                }else if (ch==']' && st.peek()!='[') {
+                    return false;
+                }else if (ch=='(' && st.peek()!=')') {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
