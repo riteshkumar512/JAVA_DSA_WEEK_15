a19bcac33b259d6880fe6722aa674c5507866f63
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-05-25 22:13:55 +0530

    Find the Celebrity

---
+++ src/Lecture_25/Reverse_Stack.java
@@ -0,0 +1,34 @@
+package Lecture_25;
+
+import java.util.Stack;
+
+public class Reverse_Stack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        System.out.println(st);
+        Reverse(st);
+        System.out.println(st);
+    }
+    public static void Reverse(Stack<Integer> st) {
+        if (st.isEmpty()){
+            return;
+        }
+        int x=st.pop();
+        Reverse(st);
+        Insert(st,x);
+    }
+    public static void Insert(Stack<Integer> st,int item){
+        if (st.isEmpty()){
+            st.push(item);
+            return;
+        }
+        int x=st.pop();
+        Insert(st,item);
+        st.push(x);
+    }
+}
